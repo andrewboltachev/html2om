d24e5790eb1ad8cee462dffbaafe79ea670c9fc1
Andrew Boltachev <andrewboltachev@gmail.com>
2016-01-20 02:24:44 +0300

    sends

--- src/cljs/html2om/core.cljs
+++ src/cljs/html2om/core.cljs
@@ -1,7 +1,10 @@
 (ns html2om.core
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+            [om.dom :as dom]
+            [clojure.string :as string]
+            [html2om.utils :as utils]
+            ))
 
 (enable-console-print!)
 
@@ -10,10 +13,44 @@
 ;; Components
 
 (defui RootView
+  static om/IQuery
+  (query [_]
+         '[:value]
+         )
+
   Object
   (render [this]
     (dom/div #js {:className "container"}
-             "Hello, world!"
+             "Enter HTML:"
+             (dom/textarea #js {:className "form-control"
+                                :style #js {:height "400px"}
+                                :value (:value
+                                         (om/props this)
+                                         )
+                                :onChange (fn [e]
+                                            (om/transact! this
+                                              `[(~'set-value
+                                                  {:value ~(.. e -target -value)}
+                                                  )]
+                                              )
+                                            )
+                                }
+                           )
+             (dom/button #js {:className "btn btn-block"
+                              :onClick (fn [e]
+                                         (om/transact! this
+                                                       `[(~'html2om
+                                                           {:html ~(:value (om/props this))}
+                                                           )]
+                                                       )
+                                         )
+                                }
+                         "C'mon!"
+                           )
+             (dom/br nil)
+             (dom/br nil)
+             (dom/pre #js {:className ""}
+                      )
              )
           )
   )
@@ -24,19 +61,59 @@
 
 (defmethod readf :default
   [{:keys [state] :as env} k params]
+  {:value "bar"}
+  )
+
+(defmethod readf :value
+  [{:keys [state] :as env} k params]
+  {:value (:value @state)}
   )
 
 (defmulti mutatef om/dispatch)
 
+(defmethod mutatef 'html2om
+  [{:keys [state] :as env} k params]
+  (println "mutatef" k params)
+  {:remote true
+   }
+  )
+
+(defmethod mutatef 'set-value
+  [{:keys [state] :as env} k {:keys [value]}]
+  {:action (fn [_]
+             (swap! state update-in [:value]
+                    (fn [old-value]
+                      value
+                      )
+                    )
+             )
+   }
+  )
+
+(defmethod readf :om-code
+  [{:keys [state] :as env} k params]
+  {:value
+   (:value @state)
+   }
+  )
+
 ;; Root
 
-(def data nil)
+(def data
+  {:value ""})
 
 (def parser (om/parser {:read readf
-                        :mutate mutatef}))
+                        :mutate mutatef
+                        }))
+
+(defn send-fn [data cb]
+  (println "send-fn" data)
+  )
 
 (def reconciler (om/reconciler
                   {:state data
+                   :parser parser
+                   :send send-fn
                    }
                   ))
 
